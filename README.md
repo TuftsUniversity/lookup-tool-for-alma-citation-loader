1e4a1ac127ccdc0535fdcf7f60fa0cd9d3f3b4c2
HSteeleLTS <44376066+HSteeleLTS@users.noreply.github.com>
2024-06-03 15:45:29 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+**Lookup Tool for Citation Loader**
+
+**Author:** Henry Steele, Library Technology Services, Tufts Univeristy, 2024
+
+**Purpose:**
+
+This tool takes a table of basic metadata such you might find in a bibliography, separated into columns such as described below, and course information described below and such as you are likely to receive in faculty emails with bibliography-style requested readings , and outputs a series of fields from Alma, from the Primo PNX Search API, the course API, and for physical items the BIB/Item API, notably the MMS ID, course code, and if applicable barcode, plus a number of fields for context. This report, aftering pulling out rows for e.g. editions you don't want, is suitable for direct upload into the citation to create citations for reading list.
+
+That cloud app and usage is described here: [https://github.com/TuftsUniversity/tufts-alma-cloudapp-citation-loader](https://github.com/TuftsUniversity/tufts-alma-cloudapp-citation-loader)
+
+**Configure:**
+
+Before hosting this app on your local web server, you need to go into api.php and enter your local instance's Primo Search, Course, and BIB API keys. They can be read-only
+
+**Input and Output:**
+
+1. Input data dictionary
+
+    1. The input sheet gathered from faculty email should have the following columns with the following characteristics
+    2. No field is required, but keep in mind fewer pieces of data will return broader , less specific results
+  1. Author
+    1. This can either be in the format "\<first\>\<space\>\<last\>" or "\<last\>\<comma\>\<any\_number of spaces\>\<first\>
+    2. If the comma is present then the script will reverse the names and remove the comma because this is what the Primo API seems to work best with
+    3. At the present it only takes the first author in a list
+    4.
+    5. For instance in the following record [https://tufts.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma991005315909703851&context=L&vid=01TUN\_INST:01TUN&lang=en&search\_scope=MyInst\_and\_CI&adaptor=Local%20Search%20Engine&isFrbr=true&tab=Everything&query=any,contains,991005315909703851&sortby=date\_d&facet=frbrgroupid,include,9076214921291068687&offset=0](https://tufts.primo.exlibrisgroup.com/discovery/fulldisplay?docid=alma991005315909703851&context=L&vid=01TUN_INST:01TUN&lang=en&search_scope=MyInst_and_CI&adaptor=Local%20Search%20Engine&isFrbr=true&tab=Everything&query=any,contains,991005315909703851&sortby=date_d&facet=frbrgroupid,include,9076214921291068687&offset=0) which is a film production of a midsummer night's dream, "Shakespeare" is the author
+    6. **Note this hasn't been tested with various other affixes a number can have like ", Jr."**
+  2. Contributor
+    1. This can either be in the format "\<first\>\<space\>\<last\>" or "\<last\>\<comma\>\<any\_number of spaces\>\<first\>
+    2. If the comma is present then the script will reverse the names and remove the comma because this is what the Primo API seems to work best with
+    3. This in the sense meant in various metadata standards, DC/MARC etc. and can include director
+    4. In the example here "Beverly Bullock" is the director so you can include this in the contributor column
+    5. Including both shakespeare as the author and the director will help narrow down the results
+    6. **Note this hasn't been tested with various other affixes a number can have like ", Jr."**
+  3. Title
+    1. The title of the work , slightly normalized for search
+  4. Publisher
+  5. Date
+  6. Course Number
+    1. This is the number that appears within the course name
+    2. It can appear anywhere in the course name , but must appear exactly as entered
+    3. So at Tufts eg FR 22 (French ) would be expressed as FR-0022
+    4. You can also include section if desired and needed to uniquely identity the section
+    5. For instance FR-0022-01
+  7. Instructor Last Name
+    1. Just for one of the insecure ideas there are multiple
+  8. Course Semester
+    1. In the format "Sp24"
+    2. This is also taken from the course name
+2. Output
+
+    1. The output metadata is taken from the Primo PNX, Course , and if it's a physical item the Bib API
+    2. The goal is to output metadata that can directly loaded into the citation loader after culling any unwanted records. ( For instance culling unwanted editions if they are returned)
+    3. The important fields are MMS ID, Course Code , Course Section, and Barcode if one exists
+    4. The other fields are informational to identify the record , including format and version
+    5. It also includes blank columns for the following metadata fields that can be included in the input for the citation loader by staff entering values for these in desired rows
+      1. Citation Type (from code table )
+        1. This is secondary type
+        2. Can be "DVD" etc
+        3. Primary type is Book for the kind of material uploaded in the citation loader. This is hard coded in app and is not included in spreadsheet
+      2. section\_info
+        1. Reading list section
+      3. Item Policy
+        1. This is the temporary item policy that will be included with the temporary library, and temporary location for physical items if a temporary move is desired
---
+++ api.php
@@ -0,0 +1,347 @@
+<?php
+header('Content-Type: application/json');
+
+ini_set('max_execution_time', 1000);
+
+$input = file_get_contents('php://input');
+$data = json_decode($input, true);
+
+if (json_last_error() !== JSON_ERROR_NONE) {
+    echo json_encode(['error' => 'Invalid JSON input']);
+    error_log('JSON decode error: ' . json_last_error_msg());
+    exit;
+}
+
+if (empty($data)) {
+    echo json_encode(['error' => 'Empty input data']);
+    error_log('Empty input data');
+    exit;
+}
+
+$apiKeyPrimo = 'PLACE PRIMO SEARCH API KEY HERE'; 
+$apiKeyCourses = 'PLACE COURSE API KEY HERE';
+$apiBib = "PLACE BIB API KEY HERE";
+
+$results = [];
+foreach ($data as $record) {
+    $apiResult = searchPrimoApi($record, $apiKeyPrimo, $apiKeyCourses, $apiBib);
+    if (!empty($apiResult)) {
+        $results = array_merge($results, $apiResult);
+    }
+
+    else{
+
+        $result[] = [
+            'Title' => 'No results for ' . $record['Title'] ?? '',
+            'Author' => 'No results for ' . $record['Author'] ?? '',
+            'Contributor' => 'No results for ' . $record['Contributor'] ?? '',
+            'Year' => 'No results for ' .  urlencode($record['Year'] ?? ''),
+            'Course Code' => urlencode($record['Course Number'] ?? ''),
+            'Format' => 'N/A'
+        ];
+        $results = array_merge($results, $result);
+
+
+
+    }
+}
+
+echo json_encode($results);
+
+function searchPrimoApi($row, $apiKey, $apiKeyCourses, $apiBib) {
+    $title = $row['Title'] ?? '';
+    $author = $row['Author'] ?? '';
+    $contributor = $row['Contributor'] ?? '';
+    $publisher = $row['Publisher'] ?? '';
+    $year = urlencode($row['Year'] ?? '');
+    $course_number = urlencode($row['Course Number'] ?? '');
+    $course_semester = urlencode($row['Course Semester'] ?? '');
+    $instructor = urlencode($row['Instructor Last Name'] ?? '');
+    
+    $query = "https://api-na.hosted.exlibrisgroup.com/primo/v1/search?q=";
+    
+    if (!empty($title)) {
+        $title =  preg_replace('/([^\:]+).*/', '$1', $title);
+        //$title = preg_replace('/\!\@\#\$\%\^\&\*\(\)\_\+\-\=\{\}\\\<\>\?\~\'/', ' ', $title);
+        $title =  preg_replace('/\s{2,}/', ' ', $title);
+
+        $query .= "title,contains," . urlencode($title);
+    }
+    if (!empty($author)) {
+        if(strpos($author, 'and') !== false){
+            $pattern = '/^([^,]+).+?and.+?$/i';
+            $replacement = '$1';
+            $author = preg_replace($pattern, $replacement, $author);
+
+        }
+        if(strpos($contributor, 'trans.') !== false || strpos($contributor, 'ed.') !== false || strpos($contributor, 'eds.') !== false ){
+            $pattern = '/(ed.\s*|trans\.\s*)(.+)/i';
+            $replacement = '$2';
+            $author = preg_replace($pattern, $replacement, $author);
+
+        }
+        if (strpos($author, ',') !== false){
+
+        
+            
+            $pattern = '/^([^,]+,\s*[^,]+$).*/i';
+            $replacement = '${1}';
+            $author =  preg_replace($pattern, $replacement, $author);
+            $author = implode(' ', array_reverse(preg_split('/,\s*/', $author)));
+        }
+        $query .= ",AND;creator,contains," . urlencode($author);
+    }
+    if (!empty($contributor)) {
+        if(strpos($contributor, 'and') !== false){
+            $pattern = '/^([^,]+).+?and.+?$/i';
+            $replacement = '$1';
+            $contributor = preg_replace($pattern, $replacement, $contributor);
+
+        }
+        if(strpos($contributor, 'trans.') !== false || strpos($contributor, 'ed.') !== false || strpos($contributor, 'eds.') !== false ){
+            $pattern = '/(ed.\s*|trans\.\s*)(.+)/i';
+            $replacement = '$2';
+            $contributor = preg_replace($pattern, $replacement, $contributor);
+
+        }
+        if (strpos($contributor, ',') !== false) {
+            $pattern = '/([^,]+,\s*[^,]+)(.*)/i';
+            $replacement = '${1}';
+            $contributor =  preg_replace($pattern, $replacement, $contributor);
+            $contributor = implode(' ', array_reverse(preg_split('/,\s*/', $contributor)));
+        }
+        $query .= ",AND;contributor,contains," . urlencode($contributor);
+    }
+    if (!empty($year)) {
+        $query .= ",AND;cdate,contains," . $year;
+    }
+    $query .= "&apikey=$apiKey&vid=01TUN_INST:01TUN&lang=en&tab=Everything&scope=MyInst_and_CI&format=json";
+
+    $response = file_get_contents($query);
+    $json = json_decode($response, true);
+
+    $results = [];
+    
+    $courseURL = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/courses?";
+    
+    if (!empty($course_number) && !empty($instructor) && !empty($course_semester)) {
+        $request_url = $courseURL . "apikey=" . $apiKeyCourses . "&q=name~" . $course_semester . "-" . $course_number . "%20AND%20instructors~" . $instructor . "&format=json";
+        $responseCourse = file_get_contents($request_url);
+        $jsonCourse = json_decode($responseCourse, true);
+    
+    
+    if (isset($json['docs']) && count($json['docs']) > 0) {
+        $real_results = false;
+        $mms_found = false;
+        foreach ($json['docs'] as $doc) {
+            if(!(isset($doc['pnx']['display']['mms']))){
+                
+                $mms_found = false;
+                
+           
+            }
+
+            else {
+
+                
+                $mms_found = true;
+            if (isset($doc['pnx']['display']['mms']) && isset($doc['delivery']['bestlocation']) && $doc['delivery']['bestlocation'] != null) {
+                $mms_id = $doc['pnx']['display']['mms'][0];
+                $itemQuery = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/$mms_id/holdings/ALL/items?apikey=$apiBib&format=json";
+                $responseItems = file_get_contents($itemQuery);
+                $items = json_decode($responseItems, true);
+                
+                if ((int)$items['total_record_count'] > 0) {
+                    foreach ($items as $item1) {
+                        if (is_array($item1)){
+                        foreach ($item1 as $item) {
+                            $real_results = true;
+                            $results[] = [
+                                'Title' => $doc['pnx']['display']['title'][0] ?? '',
+                                'Author' => $doc['pnx']['addata']['au'][0] ?? '',
+                                'Contributor' => $doc['pnx']['addata']['addau'][0] ?? '',
+                                'Publisher' => $doc['pnx']['display']['publisher'][0] ?? '',
+                                'Year' => $doc['pnx']['addata']['date'][0] ?? '',
+                                'MMS ID' => $doc['pnx']['display']['mms'][0] ?? '',
+                                'ISBN' => $doc['pnx']['addata']['isbn'][0] ?? '',
+                                'Version' => $doc['pnx']['display']['version'][0] ?? '',
+                                'Course Code' => $jsonCourse['course'][0]['code'] ?? '',
+                                'Course Section' => $jsonCourse['course'][0]['section'] ?? '',
+                                'Call Number' => $item['holding_data']['permanent_call_number'] ?? '',
+                                'Barcode' => $item['item_data']['barcode'] ?? '',
+                                'Description' => json_encode($item['item_data']['description'], true) ?? '',
+                                'Format' => 'Physical'
+                            ];
+                        }
+
+                        
+                    }
+
+                    
+                    
+
+  /*                    else{
+                         $results[] = [
+                             'Title' => $doc['pnx']['display']['title'][0] ?? '',
+                             'Author' => $doc['pnx']['addata']['au'][0] ?? '',
+                             'Contributor' => $doc['pnx']['addata']['addau'][0] ?? '',
+                             'Publisher' => $doc['pnx']['display']['publisher'][0] ?? '',
+                             'Year' => $doc['pnx']['addata']['date'][0] ?? '',
+                             'MMS ID' => $doc['pnx']['display']['mms'][0] ?? '',
+                             'ISBN' => $doc['pnx']['addata']['isbn'][0] ?? '',
+                             'Version' => $doc['pnx']['display']['version'][0] ?? '',
+                             'Course Code' => $jsonCourse['course'][0]['code'] ?? '',
+                             'Call Number' => $item1['holding_data']['permanent_call_number'] ?? '',
+                             'Barcode' => $item1['item_data']['barcode'] ?? '',
+                             'Description' => json_encode($item1['item_data']['description'], true) ?? '',
+                             'Format' => 'Physical'
+                         ];
+
+                     }
+                    */                    } 
+                }
+
+                if(isset($doc['pnx']['display']['dedupmemberids'])  && isset($doc['delivery']['bestlocation']) && $doc['delivery']['bestlocation'] != null){
+                    $mms_id = $doc['pnx']['display']['dedupmemberids'][0];
+                    $itemQuery = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/$mms_id/holdings/ALL/items?apikey=$apiBib&format=json";
+                    $responseItems = file_get_contents($itemQuery);
+
+                    $itemQuery = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/$mms_id/holdings/ALL/items?apikey=$apiBib&format=json";
+                    $responseItems = file_get_contents($itemQuery);
+                    $items = json_decode($responseItems, true);
+                    
+                    if ((int)$items['total_record_count'] > 0) {
+                        foreach ($items as $item1) {
+                            if (is_array($item1)){
+                            foreach ($item1 as $item) {
+                                $real_results = true;
+                                $results[] = [
+                                    'Title' => $doc['pnx']['display']['title'][0] ?? '',
+                                    'Author' => $doc['pnx']['addata']['au'][0] ?? '',
+                                    'Contributor' => $doc['pnx']['addata']['addau'][0] ?? '',
+                                    'Publisher' => $doc['pnx']['display']['publisher'][0] ?? '',
+                                    'Year' => $doc['pnx']['addata']['date'][0] ?? '',
+                                    'MMS ID' => $doc['pnx']['display']['dedupmemberids'][0] ?? '',
+                                    'ISBN' => $doc['pnx']['addata']['isbn'][0] ?? '',
+                                    'Version' => $doc['pnx']['display']['version'][0] ?? '',
+                                    'Course Code' => $jsonCourse['course'][0]['code'] ?? '',
+                                    'Course Section' => $jsonCourse['course'][0]['section'] ?? '',
+                                    'Call Number' => $item['holding_data']['permanent_call_number'] ?? '',
+                                    'Barcode' => $item['item_data']['barcode'] ?? '',
+                                    'Description' => json_encode($item['item_data']['description'], true) ?? '',
+                                    'Format' => 'Physical'
+                                ];
+                            }
+    
+                            
+                        }
+    
+                        
+                        
+    
+                        // else{
+                        //     $results[] = [
+                        //         'Title' => $doc['pnx']['display']['title'][0] ?? '',
+                        //         'Author' => $doc['pnx']['addata']['au'][0] ?? '',
+                        //         'Contributor' => $doc['pnx']['addata']['addau'][0] ?? '',
+                        //         'Publisher' => $doc['pnx']['display']['publisher'][0] ?? '',
+                        //         'Year' => $doc['pnx']['addata']['date'][0] ?? '',
+                        //         'MMS ID' => $doc['pnx']['display']['mms'][0] ?? '',
+                        //         'ISBN' => $doc['pnx']['addata']['isbn'][0] ?? '',
+                        //         'Version' => $doc['pnx']['display']['version'][0] ?? '',
+                        //         'Course Code' => $jsonCourse['course'][0]['code'] ?? '',
+                        //         'Call Number' => $item1['holding_data']['permanent_call_number'] ?? '',
+                        //         'Barcode' => $item1['item_data']['barcode'] ?? '',
+                        //         'Description' => json_encode($item1['item_data']['description'], true) ?? '',
+                        //         'Format' => 'Physical'
+                        //     ];
+    
+                        // }
+                        }
+                    }
+
+
+                            
+                }
+
+               
+               
+            }
+            
+            if (isset($doc['pnx']['display']['mms_id']) && isset($doc['delivery']['link'][0]['linkURL'])) {
+                $real_results = true;
+                $results[] = [
+                    'Title' => $doc['pnx']['display']['title'][0] ?? '',
+                    'Author' => $doc['pnx']['addata']['au'][0] ?? '',
+                    'Contributor' => $doc['pnx']['addata']['addau'][0] ?? '',
+                    'Publisher' => $doc['pnx']['display']['publisher'][0] ?? '',
+                    'Year' => $doc['pnx']['addata']['date'][0] ?? '',
+                    'MMS ID' => $doc['pnx']['display']['mms'][0] ?? '',
+                    'ISBN' => $doc['pnx']['addata']['isbn'][0] ?? '',
+                    'URL' => json_encode($doc['delivery']['link'][0]['linkURL']) ?? '',
+                    'Version' => $doc['pnx']['display']['version'][0] ?? '',
+                    'Course Code' => $jsonCourse['course'][0]['code'] ?? '',
+                    'Course Section' => $jsonCourse['course'][0]['section'] ?? '',
+                    'Format' => 'Electronic'];
+            }
+            else {
+                $real_results = true;
+                $results[] = [
+                    'Title' => $doc['pnx']['display']['title'][0] ?? '',
+                    'Author' => $doc['pnx']['addata']['au'][0] ?? '',
+                    'Contributor' => $doc['pnx']['addata']['addau'][0] ?? '',
+                    'Publisher' => $doc['pnx']['display']['publisher'][0] ?? '',
+                    'Year' => $doc['pnx']['addata']['date'][0] ?? '',
+                    'MMS ID' => $doc['pnx']['display']['mms'][0] ?? '',
+                    'ISBN' => $doc['pnx']['addata']['isbn'][0] ?? '',
+                    'URL' => json_encode($doc['delivery']['link'][0]['linkURL']) ?? '',
+                    'Version' => $doc['pnx']['display']['version'][0] ?? '',
+                    'Course Code' => $jsonCourse['course'][0]['code'] ?? '',
+                    'Course Section' => $jsonCourse['course'][0]['section'] ?? '',
+                    'Format' => 'Electronic'
+                ];
+            }
+        }
+            
+
+            
+        }
+
+    if($mms_found == false && $real_results = false){
+            
+        $results[] = [
+            'Title' => "No MMS ID for " . $doc['pnx']['display']['title'][0] ?? '',
+            'Author' => "No MMS ID for " . $doc['pnx']['addata']['au'][0] ?? '',
+            'Contributor' => $doc['pnx']['addata']['addau'][0] ?? '',
+            'Publisher' => "No MMS ID for " . $doc['pnx']['display']['publisher'][0] ?? '',
+            'Year' => "No MMS ID for " . $doc['pnx']['addata']['date'][0] ?? '',
+            'MMS ID' => "No MMS ID for " . $doc['pnx']['display']['mms'][0] ?? '',
+            'ISBN' => "No MMS ID for " . $doc['pnx']['addata']['isbn'][0] ?? '',
+            'Version' => "No MMS ID for " . $doc['pnx']['display']['version'][0] ?? '',
+            'Course Code' => "No MMS ID for " . $jsonCourse['course'][0]['code'] ?? '',
+            'Course Section' => $jsonCourse['course'][0]['section'] ?? '',
+            'Format' => "No MMS ID for " . 'Unknown'
+        ];
+        
+        
+    
+    }
+     }
+
+    else {
+        $results[] = [
+            'Title' => 'No results for ' . $title ?? '',
+            'Author' => 'No results for ' . $author ?? '',
+            'Contributor' => 'No results for ' . $contributor ?? '',
+            'Publisher' => 'No results for ' . $publisher ?? '',
+            'Year' => 'No results for ' . $year ?? '',
+            'Course Code' => $jsonCourse['course'][0]['code'] ?? '',
+            'Format' => 'N/A'
+        ];
+    }
+
+    return $results;
+}
+}
+
+?>
