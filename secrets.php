dab257cdc9198a2794b1ec0b2d487cca4df63256
HSteeleLTS <44376066+HSteeleLTS@users.noreply.github.com>
2024-07-24 13:11:43 -0400

    WORKING v10 with SRU instead of Primo API

---
+++ secrets.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    
+    'API_KEY_COURSES' => 'l7xxde379ecb50e14de0959be6c41c1f6888',
+    'API_BIB' => 'l8xxa16e86330d384b0c9171da72a6536dc8',
+];
+?>
---
+++ secrets_sample.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    
+    'API_KEY_COURSES' => 'your_courses_api_key',
+    'API_BIB' => 'your_bib_api_key',
+];
+?>
